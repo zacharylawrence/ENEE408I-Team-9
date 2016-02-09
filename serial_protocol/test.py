ee4dcd4fba6577d92a3109880c57e1fbafdae15e
Zachary Lawrence <zacharyclawrence@gmail.com>
2016-02-09 21:35:44 +0000

    Working on protocol

--- serial_protocol/test.py
+++ serial_protocol/test.py
@@ -5,34 +5,43 @@ import struct
 
 def establishConnection():
   # Define Constants
-  SERIAL_DEVICE = "/dev/tty.usbmodem1421"
+  SERIAL_DEVICE = "/dev/ttyACM0"
 
   # Establish Connection
   ser = serial.Serial(SERIAL_DEVICE, 9600)
   time.sleep(2)
   print("Connection Established")
 
+  return ser
+
 # Each motor speed is a float from -1.0 to 1.0
-def sendDrive(left, right):
+def sendDrive(ser, left, right):
   if(left < -1 or left > 1 or right < -1 or right > 1):
     print("Incorrectly formated drive command!")
     return;
 
-  message = bytearray(9)
+  ser.write('1')
+  #ser.write('0' if left >= 0 else '1')
+  #ser.write(struct.pack("B", abs(left) * 255))
+  #ser.write('0' if right >= 0 else '1')
+  #ser.write(struct.pack("B", abs(right) * 255))
 
-  message[0] = 1
-  message[1] = 0 if left >= 0 else 1
-  message[2] = struct.pack("B", abs(left) * 255)
-  message[3] = 0 if right >= 0 else 1
-  message[4] = struct.pack("B", abs(right) * 255)
+  ser.write('0')
+  ser.write(bytes(255))
+  ser.write('0')
+  ser.write(bytes(255))
 
-  print binascii.hexlify(message)
-  # ser.write(message)
+  ser.write('0')
+  ser.write('0')
+  ser.write('0')
+  ser.write('0')
+
+  print('test')
 
 if __name__ == '__main__':
-  # establishConnection()
-  sendDrive(-1.0, -1.0)
+  ser = establishConnection()
+  sendDrive(ser, -1.0, -1.0)
   time.sleep(5)
-  sendDrive(1.0, 1.0)
+  sendDrive(ser, 1.0, 1.0)
   time.sleep(5)
-  sendDrive(0.0, 0.0)
+  sendDrive(ser, 0.0, 0.0)

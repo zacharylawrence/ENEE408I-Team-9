969be5ed39f14cf32802f4314b04a4134a77f4ac
Zachary Lawrence <zacharyclawrence@gmail.com>
2016-02-15 17:11:00 -0500

    Create loop framework for driver.py

--- firmata/pymata-aio/driver.py
+++ firmata/pymata-aio/driver.py
@@ -4,17 +4,34 @@
 Drive the Robot
 """
 
+import sched, time
 from arduino import Arduino
 
-def main():
-  arduino = Arduino()
 
-  # arduino.blink_led()
-  # arduino.set_motors(0.25, -0.25)
-  print("Ping: " + str(arduino.get_ping()))
-  # arduino.get_ping()
+class Driver():
+  def __init__(self, looprate):
+    self.arduino = Arduino()
+    self.looprate = looprate
 
-  arduino.shutdown()
+  def loop(self, sc):
+    print("New Loop")
+    self.arduino.blink_led()
+    # self.arduino.set_motors(0.25, -0.25)
+    # print("Ping: " + str(self.arduino.get_ping()))
+    # self.arduino.get_ping()
+
+    # Loop again after delay
+    sc.enter(self.looprate, 1, self.loop, (sc,))
+
+  def shutdown(self):
+    self.arduino.shutdown()
+
 
 if __name__ == '__main__':
-  main()
+  # Wait .1 sec between loops
+  # This waits after the loop is run. This will not subtract the time it took to run loop() from the total wait time.
+  driver = Driver(.1)
+
+  s = sched.scheduler(time.time, driver.arduino.board.sleep)
+  s.enter(0, 1, driver.loop, (s,))
+  s.run()

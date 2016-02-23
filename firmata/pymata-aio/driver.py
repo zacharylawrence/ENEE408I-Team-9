8a459aba9bd20e59e016ad9b9881e0b84b2df2e6
Zachary Lawrence <zacharyclawrence@gmail.com>
2016-02-23 19:10:51 +0000

    Start working on navigation

--- firmata/pymata-aio/driver.py
+++ firmata/pymata-aio/driver.py
@@ -5,12 +5,15 @@ Drive the Robot
 """
 
 import sched, time
+import signal, sys
 from arduino import Arduino
+from navigation import Navigation
 
 
 class Driver():
   def __init__(self, looprate):
     self.arduino = Arduino()
+    self.navigation = Navigation()
     self.looprate = looprate
 
   def loop(self, sc):
@@ -21,17 +24,25 @@ class Driver():
     print("Ping: " + str(self.arduino.get_ping()))
     # self.arduino.get_ping()
 
+    (left_motor, right_motor) = self.navigation.hold_ping(self.arduino.get_ping())
+
+    print("L: " + str(left_motor) + " R: " + str(right_motor))
+
     # Loop again after delay
     sc.enter(self.looprate, 1, self.loop, (sc,))
 
-  def shutdown(self):
+  def shutdown(self, signal, frame):
     self.arduino.shutdown()
+    sys.exit(0)
 
 
 if __name__ == '__main__':
   # Wait .05 sec between loops
   # This waits after the loop is run. This will not subtract the time it took to run loop() from the total wait time.
   driver = Driver(.05)
+
+  # Graceful shutdown on ctrl-c
+  signal.signal(signal.SIGINT, driver.shutdown)
 
   s = sched.scheduler(time.time, driver.arduino.board.sleep)
   s.enter(0, 1, driver.loop, (s,))
--- firmata/pymata-aio/navigation.py
+++ firmata/pymata-aio/navigation.py
@@ -4,8 +4,25 @@
 Control Navigation Features
 """
 
+# from pixy import *
+
 class Navigation():
   def __init__(self):
+    # TODO
+    return None
 
-  with_pixy(pixy_data):
+  def with_pixy(self, pixy_data):
+    # TODO
+    return None
 
+  # Return left/right motor speeds
+  def hold_ping(self, distance):
+    if (distance == None):
+      return (0, 0)
+
+    if (distance < 20):
+      return (-0.1, -0.1)
+    elif (distance > 20):
+      return (0.1, 0.1)
+    else:
+      return (0, 0)
